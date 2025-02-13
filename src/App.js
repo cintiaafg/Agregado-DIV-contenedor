b64023399177398006a921bdb49d0adceb230af0
cintiaafg <cintia.flores8361@alumnos.udg.mx>
2025-02-12 21:14:24 -0600

    Actualizar mensajes de prueba en App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo, pruebas
-          
-          tercer prueba <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="style"></div>
+      <h1>My First React App</h1>
+      <p>Welcome to my first React app!</p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
