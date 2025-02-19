6a4686461fa670fdf322854a863ade42612a96c5
cintiaafg <cintia.flores8361@alumnos.udg.mx>
2025-02-19 17:56:17 -0600

    #commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img 
-      className="imagen_perfil"
-      src="https://cdn.pixabay.com/photo/2022/06/05/07/04/person-7243410_1280.png" alt="perfil" />
+      <div className='container'>
+        <img className="imagen_perfil" src="https://cdn.pixabay.com/photo/2022/06/05/07/04/person-7243410_1280.png" alt="perfil"/>
+      </div>
     </div>
   );
 }
