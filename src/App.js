02628009c72113f155f92153c3574d79732f3b61
cintiaafg <cintia.flores8361@alumnos.udg.mx>
2025-02-19 17:37:59 -0600

    #commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <img 
       className="imagen_perfil"
-      src="https://cdn6.aptoide.com/imgs/1/d/1/1d13f0f2d665f45acd9c53f477928d99_icon.png" alt="perfil" />
+      src="https://cdn.pixabay.com/photo/2022/06/05/07/04/person-7243410_1280.png" alt="perfil" />
     </div>
   );
 }
